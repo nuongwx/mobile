876f247ef46331d8ee61a974a9e326ddb94d017b
vmathu <anhthuvominh1@gmail.com>
2024-03-31 13:21:41 +0700

    DT-11: feat: setup app bar and fragments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,15 +32,19 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.legacy.support.v4)
+    implementation(libs.androidx.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
