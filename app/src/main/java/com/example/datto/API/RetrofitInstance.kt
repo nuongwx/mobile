4ca443f0f467b278ccb7354877454ce8e99ddcf2
ducviet5138 <ducviet5138@gmail.com>
2024-04-17 23:10:52 +0700

    DT-45: Fix: Cache time, redundant preloaded avatar

--- app/src/main/java/com/example/datto/API/RetrofitInstance.kt
+++ app/src/main/java/com/example/datto/API/RetrofitInstance.kt
@@ -2,7 +2,7 @@ package com.example.datto.API
 
 import android.content.Context
 import android.net.ConnectivityManager
-import android.net.NetworkInfo
+import android.net.NetworkCapabilities
 import com.example.datto.GlobalVariable.GlobalVariable
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
@@ -12,17 +12,17 @@ class RetrofitInstance {
     private val BASE_URL: String = GlobalVariable.BASE_URL
     private val cacheSize = (50 * 1024 * 1024).toLong() // 50 MB
 
-    private fun hasNetwork(context: Context): Boolean? {
-        var isConnected: Boolean? = false // Initial Value
+    private fun hasNetwork(context: Context): Boolean {
         val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
-        if (activeNetwork != null && activeNetwork.isConnected)
-            isConnected = true
-        return isConnected
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        return networkCapabilities != null && (
+                networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                        networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR))
     }
 
     private fun getCacheInstance(context: Context): OkHttpClient {
         val myCache = okhttp3.Cache(context.cacheDir, cacheSize)
+        val maxAge = 60 * 60 * 24 * 7 // 1 week
 
         val okHttpClient = OkHttpClient.Builder()
             // Specify the cache we created earlier.
@@ -38,14 +38,14 @@ class RetrofitInstance {
                 *  we initialize the request and change its header depending on whether
                 *  the device is connected to Internet or not.
                 */
-                request = if (hasNetwork(context)!!)
+                request = if (hasNetwork(context))
                 /*
                 *  If there is Internet, get the cache that was stored 5 seconds ago.
                 *  If the cache is older than 5 seconds, then discard it,
                 *  and indicate an error in fetching the response.
                 *  The 'max-age' attribute is responsible for this behavior.
                 */
-                    request.newBuilder().header("Cache-Control", "public, max-age=" + 5).build()
+                    request.newBuilder().header("Cache-Control", "public, max-age=$maxAge").build()
                 else
                 /*
                 *  If there is no Internet, get the cache that was stored 7 days ago.
@@ -54,7 +54,7 @@ class RetrofitInstance {
                 *  The 'max-stale' attribute is responsible for this behavior.
                 *  The 'only-if-cached' attribute indicates to not retrieve new data; fetch the cache only instead.
                 */
-                    request.newBuilder().header("Cache-Control", "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 7).build()
+                    request.newBuilder().header("Cache-Control", "public, only-if-cached, max-stale=$maxAge").build()
                 // End of if-else statement
 
                 // Add the modified request to the chain.
@@ -62,8 +62,9 @@ class RetrofitInstance {
             }
             .addNetworkInterceptor { chain ->
                 // Add an Interceptor to the OkHttpClient.
+                // Always serve the response from the cache first
+                // Update the cache with the response from the server later
                 val response = chain.proceed(chain.request())
-                val maxAge = 60 // read from cache for 60 secs even if there is internet connection
                 response.newBuilder()
                     .header("Cache-Control", "public, max-age=$maxAge")
                     .removeHeader("Pragma")
