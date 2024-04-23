95f281895d2cd1d2ad7c3fd9a8c47544cc3698e3
ducviet5138 <ducviet5138@gmail.com>
2024-04-23 08:39:48 +0700

    DT-45: Feat: Load cache when server down

--- app/src/main/java/com/example/datto/API/RetrofitInstance.kt
+++ app/src/main/java/com/example/datto/API/RetrofitInstance.kt
@@ -26,47 +26,36 @@ class RetrofitInstance {
     private fun getCacheInstance(context: Context): OkHttpClient {
         val myCache = okhttp3.Cache(context.cacheDir, cacheSize)
         val maxAge = 60 * 60 * 24 * 7 // 1 week
+        val timeout = 10L // 10 seconds
 
         val okHttpClient = OkHttpClient.Builder()
             // Specify the cache we created earlier.
             .cache(myCache)
+            // Set the timeout for the OkHttpClient.
+            .connectTimeout(timeout, TimeUnit.SECONDS)
+            .readTimeout(timeout, TimeUnit.SECONDS)
+            .writeTimeout(timeout, TimeUnit.SECONDS)
             // Add an Interceptor to the OkHttpClient.
             .addInterceptor { chain ->
 
                 // Get the request from the chain.
                 var request = chain.request()
 
-                /*
-                *  Leveraging the advantage of using Kotlin,
-                *  we initialize the request and change its header depending on whether
-                *  the device is connected to Internet or not.
-                */
                 request = if (hasNetwork(context))
-                /*
-                *  If there is Internet, get the cache that was stored 5 seconds ago.
-                *  If the cache is older than 5 seconds, then discard it,
-                *  and indicate an error in fetching the response.
-                *  The 'max-age' attribute is responsible for this behavior.
-                */
                     request.newBuilder().header("Cache-Control", "public, max-age=5").build()
                 else
-                /*
-                *  If there is no Internet, get the cache that was stored 7 days ago.
-                *  If the cache is older than 7 days, then discard it,
-                *  and indicate an error in fetching the response.
-                *  The 'max-stale' attribute is responsible for this behavior.
-                *  The 'only-if-cached' attribute indicates to not retrieve new data; fetch the cache only instead.
-                */
                     request.newBuilder().header("Cache-Control", "public, only-if-cached, max-stale=$maxAge").build()
-                // End of if-else statement
 
-                // Add the modified request to the chain.
-                chain.proceed(request)
+                try {
+                    // Try to proceed with the request.
+                    chain.proceed(request)
+                } catch (e: Exception) {
+                    // If the request times out, proceed with the cached response.
+                    request = request.newBuilder().header("Cache-Control", "public, only-if-cached").build()
+                    chain.proceed(request)
+                }
             }
             .addNetworkInterceptor { chain ->
-                // Add an Interceptor to the OkHttpClient.
-                // Always serve the response from the cache first
-                // Update the cache with the response from the server later
                 val response = chain.proceed(chain.request())
                 response.newBuilder()
                     .header("Cache-Control", "public, max-age=$maxAge")
