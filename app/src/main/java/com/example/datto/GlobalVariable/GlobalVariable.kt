2d64f469190ec365f2c0d60da838e0c69b20cef9
ducviet5138 <99721942+ducviet5138@users.noreply.github.com>
2024-05-05 22:24:58 +0700

    Change API url

--- app/src/main/java/com/example/datto/GlobalVariable/GlobalVariable.kt
+++ app/src/main/java/com/example/datto/GlobalVariable/GlobalVariable.kt
@@ -1,5 +1,5 @@
 package com.example.datto.GlobalVariable
 
 object GlobalVariable {
-    const val BASE_URL: String = "http://192.168.1.6:3000/api/"
+    const val BASE_URL: String = "http://18.141.208.69:3000/api/"
 }
