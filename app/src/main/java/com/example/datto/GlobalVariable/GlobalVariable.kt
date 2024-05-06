20e8f7009fe8a4e36ace9e0adec2fd5b431822cd
ducviet5138 <99721942+ducviet5138@users.noreply.github.com>
2024-05-06 08:53:22 +0700

    Revert API URL

--- app/src/main/java/com/example/datto/GlobalVariable/GlobalVariable.kt
+++ app/src/main/java/com/example/datto/GlobalVariable/GlobalVariable.kt
@@ -1,5 +1,5 @@
 package com.example.datto.GlobalVariable
 
 object GlobalVariable {
-    const val BASE_URL: String = "https://polary.pt/api/"
+    const val BASE_URL: String = "http://18.141.208.69:3000/api/"
 }
